8162de782e02efb06420090aaf334ec764f7c991
zhuanghaixin <649308293@qq.com>
2020-12-09 14:59:59 +0800

    feat: day01-配置全局less style-resource-loader

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,12 @@ export default {
 </script>
 <style lang="less">
 // todo 1-3  当前设计稿是750px  所以 1rem就是屏幕宽度的十分之一 75px  fontSize就是75px
-//  .rem(@height,@value){
-//    @{height}:@value/75
-//  }
+  .rem(@height,@value){
+    @{height}:@value/75
+  }
   .box{
     background-color: #f00;
-    //.rem(width,375);
-    //.rem(height,75);
+    .rem(width,375);
+    .rem(height,88);
   }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,8 @@ const autoprefixer = require('autoprefixer')
 const pxtorem = require('postcss-pxtorem')
 //  todo 1-4 转换rem  当前像素  / 设计稿像素/10    设计稿是375像素
 
+// todo 1-6 全局less
+const path = require('path')
 // 暴露出去
 module.exports = {
   // css属性如何处理
@@ -15,13 +17,24 @@ module.exports = {
       postcss: {
         plugins: [
           autoprefixer(),
-          // 使用 pxtorem库进行转换
+          // 使用 pxtorem库进行转换  设计稿像素/10
           pxtorem({
             rootValue: 37.5,
             propList: ['*']
           })
         ]
       }
+    }
+  },
+
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [
+        // 项目根路径下 /src/styles/ 的所有less文件
+        // 指定某一个的话可以具体设置为某一个less文件
+        path.resolve(__dirname, './src/style/*.less')
+      ]
     }
   }
 }

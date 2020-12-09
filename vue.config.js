779d59364c903b7d003f3ff0aa601d1e2a5cdf1e
zhuanghaixin <649308293@qq.com>
2020-12-09 15:57:01 +0800

    feat: day01-配置全局less style-resource-loader

--- vue.config.js
+++ vue.config.js
@@ -33,7 +33,7 @@ module.exports = {
       patterns: [
         // 项目根路径下 /src/styles/ 的所有less文件
         // 指定某一个的话可以具体设置为某一个less文件
-        path.resolve(__dirname, './src/style/*.less')
+        path.resolve(__dirname, './src/style/global/*.less')
       ]
     }
   }

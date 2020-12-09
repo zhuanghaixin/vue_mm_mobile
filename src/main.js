d9925c48d4a170529ca8920856dfc0d88d6cac4b
zhuanghaixin <649308293@qq.com>
2020-12-09 11:37:06 +0800

    feat: day01-安装amfe-flexible插件

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入vant
 import Vant from 'vant'
 import 'vant/lib/index.css'
+// 引入 amfe-flexible
+import 'amfe-flexible'
 Vue.use(Vant)
 Vue.config.productionTip = false
 

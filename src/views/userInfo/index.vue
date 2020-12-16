4e6dd5f21b0cd428c5ed1c661fe956938f602e7b
zhuanghaixin <649308293@qq.com>
2020-12-16 23:58:28 +0800

    refactor: day06-van-row重构userInfo页面

--- src/views/userInfo/index.vue
+++ src/views/userInfo/index.vue
@@ -1,34 +1,34 @@
 <template>
   <div class="userInfo">
     <div class="top">
-      <div class="top-1">
-        <div class="left">
+      <van-row class="top-1" justify="space-between" align="center">
+        <van-col span="20" class="left">
           <h2>{{ getUserInfo.nickname }}</h2>
           <p>{{ getUserInfo.intro }}</p>
-        </div>
-        <div class="right" @click="navToInfo">
+        </van-col>
+        <van-col span="4" class="right" @click="navToInfo">
           <img v-if="getUserInfo.avatar" :src="baseURL+getUserInfo.avatar" alt="">
           <img v-else src="@/assets/logo.png" alt="">
-        </div>
-      </div>
-      <div class="top-2">
-        <div class="top-2-item">
+        </van-col>
+      </van-row>
+      <van-row class="top-2" justify="space-between">
+        <van-col span="6" class="top-2-item">
           <h3>{{ getUserInfo.submitNum }}</h3>
           <span>累计错题</span>
-        </div>
-        <div class="top-2-item">
+        </van-col>
+        <van-col span="6" class="top-2-item">
           <h3>{{ getUserInfo.collectQuestions.length }}</h3>
           <span>收藏答题</span>
-        </div>
-        <div class="top-2-item">
+        </van-col>
+        <van-col span="6" class="top-2-item">
           <h3>{{ getUserInfo.errorQuestions.length }}</h3>
           <span>我的错题</span>
-        </div>
-        <div class="top-2-item">
-          <h3>{{rightPercent}}</h3>
+        </van-col>
+        <van-col span="6" class="top-2-item">
+          <h3>{{ rightPercent }}</h3>
           <span>正确率</span>
-        </div>
-      </div>
+        </van-col>
+      </van-row>
       <div class="top-3">
         <Cell
           class="job-cell"
@@ -47,8 +47,8 @@
             <h3>面经数据</h3>
           </template>
           <template #label>
-            <ul class="interview-data-content">
-              <li class="interview-data-item">
+            <van-row justif="space-between" gutter="20" class="interview-data-content">
+              <van-col span="8" class="interview-data-item">
                 <div class="item-top">昨日阅读<span>+{{ getUserInfo.shareData.read.yesterday }}</span></div>
                 <div class="item-middle">{{
                     getUserInfo.shareData.read.total
@@ -57,24 +57,23 @@
                 <div class="item-bottom">
                   阅读总数
                 </div>
-              </li>
-              <li class="interview-data-item">
+              </van-col>
+              <van-col span="8" class="interview-data-item">
                 <div class="item-top">昨日获赞<span>+{{ getUserInfo.shareData.star.yesterday }}</span></div>
                 <div class="item-middle">{{ getUserInfo.shareData.star.total }}</div>
                 <div class="item-bottom">
                   获赞总数
                 </div>
-              </li>
-              <li class="interview-data-item">
+              </van-col>
+              <van-col span="8" class="interview-data-item">
                 <div class="item-top">昨日新增<span>+{{ getUserInfo.shareData.comment.yesterday }}</span></div>
                 <div class="item-middle">{{ getUserInfo.shareData.comment.total }}</div>
                 <div class="item-bottom">
                   评论总数
                 </div>
-              </li>
-            </ul>
+              </van-col>
+            </van-row>
           </template>
-
         </van-cell>
       </div>
       <div class="options">
@@ -86,9 +85,7 @@
         <Cell icon="&#xe611;" title="收藏的面试经验" value="32"></Cell>
       </div>
     </div>
-
   </div>
-
 </template>
 
 <script>
@@ -120,7 +117,6 @@ export default {
       baseURL: process.env.VUE_APP_URL // 基地址
     }
   },
-
   created () {
 
   },
@@ -131,40 +127,38 @@ export default {
     },
     // todo 5-3-1 跳转 编辑用户昵称
     goEditInfo (typeName) {
-      this.$router.push({ name: 'EditInfo', query: { type: typeName } })
+      this.$router.push({
+        name: 'EditInfo',
+        query: { type: typeName }
+      })
     }
   }
 
 }
 </script>
-
 <style lang="less" scoped>
 .userInfo {
   background-color: @color-bg;
   height: calc(100% - 60px);
-
   .top {
     color: @color-white;
+    padding: 0 @padding-15;
     background: @color-red-primary;
     height: 215.5px;
     position: relative;
-    padding: 0 @padding-15;
     padding-top: 0.1px;
-
     .top-1 {
       margin-top: 31px;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-
       .left {
+        height: 50px;
+        display: flex;
+        flex-direction: column;
+        justify-content:space-between;
         h2 {
           font-size: 21px;
           font-family: PingFangSC, PingFangSC-Semibold;
           font-weight: 600;
-          margin-bottom: 6px;
         }
-
         p {
           opacity: 0.7;
           font-size: 12px;
@@ -172,14 +166,11 @@ export default {
           font-weight: 400;
         }
       }
-
       .right {
-
         border: 3px solid rgba(255, 255, 255, 0.38);
         height: 50px;
         width: 50px;
         border-radius: 50%;
-
         img {
           border-radius: 50%;
           width: 100%;
@@ -188,24 +179,18 @@ export default {
         }
       }
     }
-
     .top-2 {
-      display: flex;
-      justify-content: space-between;
       margin-top: 31px;
-      padding: 0 @padding-15;
-
+      //padding: 0 @padding-15;
       .top-2-item {
         display: flex;
         flex-direction: column;
         align-items: center;
-
         h3 {
           font-size: 21px;
           font-weight: 700;
           margin-bottom: 6px;
         }
-
         span {
           opacity: 0.7;
           font-size: 12px;
@@ -213,53 +198,39 @@ export default {
           font-weight: 400;
         }
       }
-
     }
-
     .top-3 {
       padding: 0 @padding-15;
       position: absolute;
       left: 0;
       width: 100%;
       bottom: -16px;
-
       .job-cell {
         border: 1px solid transparent;
         border-radius: 8px;
         overflow: hidden;
       }
-
     }
-
   }
-
   .middle {
     padding: 0 @padding-15;
-
     .interview-data {
       margin-top: 27px;
       border: 1px solid transparent;
       overflow: hidden;
       border-radius: 8px;
-
       .interview-data-content {
-        display: flex;
-        justify-content: space-between;
-
         .interview-data-item {
           text-align: center;
-
           .item-top {
             margin-top: 17px;
             font-size: 10px;
             color: @color-gray;
             margin-bottom: 10px;
-
             span {
               color: @color-blue;
             }
           }
-
           .item-middle {
             font-size: 21px;
             font-family: DINAlternate, DINAlternate-Bold;
@@ -268,7 +239,6 @@ export default {
             text-align: center;
             margin-bottom: 6px;
           }
-
           .item-bottom {
             font-size: 12px;
             font-family: PingFangSC, PingFangSC-Regular;
@@ -277,9 +247,7 @@ export default {
           }
         }
       }
-
     }
-
     .options {
       margin-top: 11px;
       border: 1px solid transparent;
